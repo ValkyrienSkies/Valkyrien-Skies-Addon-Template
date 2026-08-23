013c38d89fba41dde156b63e2caa71bc147c7e89
blockninja124 <64211894+blockninja124@users.noreply.github.com.>
2026-08-22 20:34:17 -0400

    Convert to fabric-only

---
+++ src/main/java/org/valkyrienskies/vs_template/mixin/mixins.md
@@ -0,0 +1,3 @@
+## What is this?
+Mixins are special classes used to modify existing classes in other projects!
+For more information, check the [fabric mixin wiki](https://wiki.fabricmc.net/tutorial:mixin_introduction)
---
+++ src/main/kotlin/org/valkyrienskies/vs_template/client/VSTemplateModClient.kt
@@ -0,0 +1,10 @@
+package org.valkyrienskies.vs_template.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class VSTemplateModClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+        // put anything initialized on client here
+    }
+}
